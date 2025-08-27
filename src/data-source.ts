2e728110efe23d83f3afa1d498109f593919311a
Eliud Sosa <eliud.sosa97@gmail.com>
2025-08-27 01:52:21 -0600

    fix: resolve alias

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,17 +1,38 @@
+// src/data-source.ts
 import 'dotenv/config';
 import { DataSource, DataSourceOptions } from 'typeorm';
 
-export const dataSourceOptions: DataSourceOptions = {
-  type: 'postgres',
-  host: process.env.DATABASE_HOST,
-  port: parseInt(process.env.DATABASE_PORT || '5432'),
-  username: process.env.DATABASE_USERNAME,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE_NAME,
-  entities: ['dist/**/*.entity.js'],
-  migrations: ['dist/database/migrations/*.js'],
-  synchronize: false,
+const getDataSourceOptions = (): DataSourceOptions => {
+  const dbUrl = process.env.DATABASE_URL;
+
+  if (dbUrl) {
+    return {
+      type: 'postgres',
+      url: dbUrl,
+      synchronize: false,
+      logging: false,
+      entities: ['dist/**/*.entity.js'],
+      migrations: ['dist/database/migrations/*.js'],
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    };
+  } else {
+    return {
+      type: 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: parseInt(process.env.DATABASE_PORT || '5432'),
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      synchronize: false,
+      logging: true,
+      entities: ['src/**/*.entity.ts'],
+      migrations: ['src/database/migrations/*.ts'],
+    };
+  }
 };
 
+const dataSourceOptions = getDataSourceOptions();
 const dataSource = new DataSource(dataSourceOptions);
 export default dataSource;
